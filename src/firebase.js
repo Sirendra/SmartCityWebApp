c64dc92b56fa02ad8802419fc3c74646a886467e
jerry <Sirendra99@gmail.com>
2021-10-06 13:20:56 +0700

    Intial Commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase'
 
 var config = {
-  apiKey: "AIzaSyAcBke-3zk363Ml8i_1YYRGDluKeB9_W7I",
-  authDomain: "final-project-4c167.firebaseapp.com",
-  projectId: "final-project-4c167",
-  storageBucket: "final-project-4c167.appspot.com",
-  messagingSenderId: "573930368307",
-  appId: "1:573930368307:web:8d2c157f785348506d2246"
+your firebase config
 };
   
 const fb = firebase.initializeApp(config);
